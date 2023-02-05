d43638840740981e09798143b8ed2d0f96e9c763
Fabian C. Moss <fabianmoss@gmail.com>
2023-02-05 12:56:55 +0100

    update figure

--- src/chains.py
+++ src/chains.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 rng = np.random.default_rng()
 
-N = 20
-t_max = 30
+N = 5
+t_max = 13
 
 population = pd.Series(
     rng.choice(["A", "B"], size=N, replace=True), name="trait"
@@ -15,10 +15,9 @@ ps = pd.Series([np.nan] * t_max, name="p")
 ps[0] = population.value_counts(normalize=True,dropna=False).sort_index()[0]
 
 colors = {"A" : "darkgrey", 
-            "B": "salmon"}
+          "B" : "salmon"}
 
-_, axes = plt.subplots(2,1, sharex=True)
-axes[0].axis("equal")
+_, axes = plt.subplots(2,1, sharex=False, figsize=(t_max,N * 1.5), height_ratios=[3,1])
 
 circle_kws = dict(
     edgecolor="black", 
@@ -42,23 +41,28 @@ for t in range(1, t_max):
         axes[0].add_patch(circ)
 
     # draw arrows
-    # for src, tgt in zip(population.index, range(N)):
-    #     x = 2 * t # population.index[n]
-    #     y = src
-    #     dx = 2 * (t + 1) - x
-    #     dy = tgt - src
-    #     axes[0].arrow(x,y,dx,dy, color="k", 
-    #         head_width=0.1, head_length=0.1, #overhang=2,
-    #         length_includes_head=True,
-    #         alpha=.6, lw=1, zorder=3
-    #     )
+    for src, tgt in zip(new_population.index, range(N)):
+        x = t - 1
+        y = src
+        dx = t - x
+        dy = tgt - src
+        axes[0].arrow(x,y,dx,dy, color="k", 
+            head_width=0.1, head_length=0.1, overhang=.3,
+            length_includes_head=True,
+            alpha=.6, lw=1, zorder=3
+        )
+
     ps[t] = new_population.value_counts(
         normalize=True,dropna=False).sort_index()[-1]
-    population = new_population
+    population = new_population.reset_index(drop=True)
 
-axes[0].set_xlim(-1,t_max)
-axes[0].set_ylim(-1,N + 1)
+# axes[0].axis("equal")
+axes[0].set_aspect('equal') # should be "datalim"
+# axes[1].set_aspect(2,adjustable="box") # should be "datalim"
+# axes[0].set_xlim(-1,t_max)
+# axes[0].set_ylim(-2, N + 1)
 axes[0].set_ylabel("individuals")
+axes[0].set_xticks([])
 
 axes[1].plot(range(0, t_max), ps, c="salmon", lw=3)
 axes[1].set_ylim(0,1)
