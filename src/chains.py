411ee88a076c52f73913cf3082be98f1f75d37fe
Fabian C. Moss <fabianmoss@gmail.com>
2023-02-05 12:28:28 +0100

    improve plot

--- src/chains.py
+++ src/chains.py
@@ -1,50 +1,69 @@
 import numpy as np 
-import matplotlib.pyplot as plt 
 import pandas as pd
+import matplotlib.pyplot as plt 
 
-N = 5
-t_max = 8
+rng = np.random.default_rng()
 
-scaling = 1.
+N = 20
+t_max = 30
 
-variants =["#a58d48", "#a670b0"] # optimized for color blindness with iwanthue
+population = pd.Series(
+    rng.choice(["A", "B"], size=N, replace=True), name="trait"
+)
+
+ps = pd.Series([np.nan] * t_max, name="p")
+ps[0] = population.value_counts(normalize=True,dropna=False).sort_index()[0]
+
+colors = {"A" : "darkgrey", 
+            "B": "salmon"}
 
 _, axes = plt.subplots(2,1, sharex=True)
 axes[0].axis("equal")
 
-ps = []
+circle_kws = dict(
+    edgecolor="black", 
+    linewidth=1., 
+    alpha=1, 
+    radius=.2)
 
-# grid 
-for t in range(t_max):
+# first generation
+for idx, indv in population.items():
+    circ = plt.Circle(xy=(0,idx),
+            facecolor=colors[indv], **circle_kws)
+    axes[0].add_patch(circ)
+
+# transmission
+for t in range(1, t_max):
     # draw individuals
-    colors = np.random.choice(variants, size=N)
-    ps.append(pd.Series(colors).value_counts(normalize=True).sort_index()[0])
-    for n, c in zip(range(N), colors):
-        circ = plt.Circle(xy=(2 * t,n), radius=.4, 
-                facecolor=c, edgecolor="black", linewidth=2., alpha=1)
+    new_population = population.sample(N,replace=True)
+    for n, (idx, indv) in enumerate(new_population.items()):
+        circ = plt.Circle(xy=(t,n), 
+                facecolor=colors[indv], **circle_kws)
         axes[0].add_patch(circ)
-for t in range(t_max - 1):
-    # draw transmissions
-    sources = np.random.choice(range(N), replace=True, size=N)
-    targets = range(N) # np.random.choice(range(N), replace=True, size=N)
-    for src, trg in zip(sources, targets):
-        x = 2 * t
-        y = src
-        dx = (2 * (t + 1) - x ) * scaling
-        dy = (trg - y ) * scaling
-        axes[0].arrow(x,y,dx,dy, color="k", 
-            head_width=0.1, head_length=0.2, overhang=.1,
-            length_includes_head=True
-        )
 
-axes[0].set_xlim(-1,2 * t_max - 1)
-axes[0].set_ylim(0,N - 1)
+    # draw arrows
+    # for src, tgt in zip(population.index, range(N)):
+    #     x = 2 * t # population.index[n]
+    #     y = src
+    #     dx = 2 * (t + 1) - x
+    #     dy = tgt - src
+    #     axes[0].arrow(x,y,dx,dy, color="k", 
+    #         head_width=0.1, head_length=0.1, #overhang=2,
+    #         length_includes_head=True,
+    #         alpha=.6, lw=1, zorder=3
+    #     )
+    ps[t] = new_population.value_counts(
+        normalize=True,dropna=False).sort_index()[-1]
+    population = new_population
+
+axes[0].set_xlim(-1,t_max)
+axes[0].set_ylim(-1,N + 1)
 axes[0].set_ylabel("individuals")
 
-axes[1].plot(range(0,2 * t_max, 2), ps, c="k", lw=2)
+axes[1].plot(range(0, t_max), ps, c="salmon", lw=3)
 axes[1].set_ylim(0,1)
 axes[1].set_ylabel("proportion of 'A'")
 axes[1].set_xlabel("generations")
 
 plt.tight_layout()
-plt.show()
+plt.savefig("img/chains.png", dpi=300)
